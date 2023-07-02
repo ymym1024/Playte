b55527c215559f3816ff37f0dbad4638ab6be12e
yydev10 <jym971024@gmail.com>
2023-07-02 16:37:25 +0900

    [#1] config : 프로젝트 폴더링

--- app/src/main/java/com/cmc/recipe/ui/activity/MainActivity.kt
+++ app/src/main/java/com/cmc/recipe/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.cmc.recipe
+package com.cmc.recipe.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cmc.recipe.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
