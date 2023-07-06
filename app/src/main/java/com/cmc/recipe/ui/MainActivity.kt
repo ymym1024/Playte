836622a0d2afa76228d58e2b94fe92e607f5e5be
yydev10 <jym971024@gmail.com>
2023-07-06 16:59:39 +0900

    [#3] fix : jetpack navigation 연결 로직 수정

---
+++ app/src/main/java/com/cmc/recipe/ui/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.cmc.recipe.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.cmc.recipe.R
+import com.cmc.recipe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navSetting()
+    }
+
+    private fun navSetting(){
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener {
+                _, _, _ ->
+            binding.toolbarTitle.text = navController.currentDestination?.label.toString()
+        }
+        val graph = navController.navInflater.inflate(R.navigation.nav_graph)
+
+        navController.graph = graph
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    // toolbar show flag
+    fun hideToolbar(state:Boolean){
+        if(state){
+            binding.toolbar.visibility = View.GONE
+        }else{
+            binding.toolbar.visibility = View.VISIBLE
+        }
+    }
+}
