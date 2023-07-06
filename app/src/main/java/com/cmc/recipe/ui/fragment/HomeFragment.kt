c2124fb482af292e842444e6e28d36c2965a42e4
yydev10 <jym971024@gmail.com>
2023-07-07 01:54:04 +0900

    [#3] feat : 식재료 등록 UI 구현

--- app/src/main/java/com/cmc/recipe/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/cmc/recipe/ui/fragment/HomeFragment.kt
@@ -1,22 +1,17 @@
 package com.cmc.recipe.ui.fragment
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.cmc.recipe.R
+import com.cmc.recipe.base.BaseFragment
+import com.cmc.recipe.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun initFragment() {
+        // TODO : 수정예정
+        binding.btnAdd.setOnClickListener {
+            val ingredientBottomSheet = IngredientBottomSheetFragment(requireContext())
+            ingredientBottomSheet.show(parentFragmentManager,ingredientBottomSheet.tag)
+        }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
 
 }
