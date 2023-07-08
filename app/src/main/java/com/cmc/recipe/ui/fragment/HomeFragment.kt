552179ddf15c66d439d5e84d3d9a81b6315fdc43
yydev10 <jym971024@gmail.com>
2023-07-08 18:04:14 +0900

    [#3] feat : 냉장, 실온 viewpager 구현

--- app/src/main/java/com/cmc/recipe/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/cmc/recipe/ui/fragment/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.cmc.recipe.ui.fragment
 
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.cmc.recipe.base.BaseFragment
 import com.cmc.recipe.databinding.FragmentHomeBinding
 
@@ -10,6 +12,20 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
         binding.btnAdd.setOnClickListener {
             val ingredientBottomSheet = IngredientBottomSheetFragment(requireContext())
             ingredientBottomSheet.show(parentFragmentManager,ingredientBottomSheet.tag)
+        }
+
+        // 냉장고 UI - viewPager2 연결
+        val innerFragmentList:ArrayList<Fragment> = arrayListOf(InnerViewFragment1(),InnerViewFragment2())
+        val viewPager2Adapter = RefrigeratorViewAdapter(childFragmentManager,lifecycle,innerFragmentList)
+        val viewpager = binding.vpRefrigerator
+        viewpager.adapter = viewPager2Adapter
+
+        // 냉장, 실온 클릭 시 viewpager 이동
+        binding.tvRefrigerator.setOnClickListener {
+            viewpager.currentItem = 0
+        }
+        binding.tvAmbient.setOnClickListener {
+            viewpager.currentItem = 1
         }
     }
 
