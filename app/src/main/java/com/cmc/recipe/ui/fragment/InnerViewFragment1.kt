019ff63a238abe72ace189a4a8be9e311616e3bd
yydev10 <jym971024@gmail.com>
2023-07-08 15:35:18 +0900

    [#3] chore : 냉동/냉장 fragment 추가

---
+++ app/src/main/java/com/cmc/recipe/ui/fragment/InnerViewFragment1.kt
@@ -0,0 +1,11 @@
+package com.cmc.recipe.ui.fragment
+
+
+import com.cmc.recipe.base.BaseFragment
+import com.cmc.recipe.databinding.FragmentInnerView1Binding
+
+class InnerViewFragment1 : BaseFragment<FragmentInnerView1Binding>(FragmentInnerView1Binding::inflate) {
+    override fun initFragment() {
+
+    }
+}
