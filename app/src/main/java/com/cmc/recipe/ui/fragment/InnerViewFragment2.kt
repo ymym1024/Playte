46920101221a2d8b8b58ade772363fe2a47497b4
yydev10 <jym971024@gmail.com>
2023-07-08 15:35:28 +0900

    [#3] chore : 실온 fragment 추가

---
+++ app/src/main/java/com/cmc/recipe/ui/fragment/InnerViewFragment2.kt
@@ -0,0 +1,12 @@
+package com.cmc.recipe.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.cmc.recipe.base.BaseFragment
+import com.cmc.recipe.databinding.FragmentInnerView2Binding
+
+
+class InnerViewFragment2 : BaseFragment<FragmentInnerView2Binding>(FragmentInnerView2Binding::inflate) {
+    override fun initFragment() {
+
+    }
+}
