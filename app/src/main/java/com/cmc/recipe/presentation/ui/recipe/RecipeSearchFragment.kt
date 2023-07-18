2b9986d5cc5fb0238017d38734b23b4779d46fec
yydev10 <jym971024@gmail.com>
2023-07-19 02:35:56 +0900

    [#13] style : 레시피 검색 UI 구현

---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeSearchFragment.kt
@@ -0,0 +1,16 @@
+package com.cmc.recipe.presentation.ui.recipe
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cmc.recipe.R
+import com.cmc.recipe.databinding.FragmentRecipeSearchBinding
+import com.cmc.recipe.presentation.ui.base.BaseFragment
+
+class RecipeSearchFragment : BaseFragment<FragmentRecipeSearchBinding>(FragmentRecipeSearchBinding::inflate) {
+    override fun initFragment() {
+        TODO("Not yet implemented")
+    }
+}
