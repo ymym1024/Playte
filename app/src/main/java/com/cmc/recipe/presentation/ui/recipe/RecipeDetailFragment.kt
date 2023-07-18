d27aa6c992acf42e1682fbfd55154930de21bccd
yydev10 <jym971024@gmail.com>
2023-07-19 02:22:54 +0900

    [#13] style : 레시피 상세화면 UI 구현

---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeDetailFragment.kt
@@ -0,0 +1,17 @@
+package com.cmc.recipe.presentation.ui.recipe
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cmc.recipe.R
+import com.cmc.recipe.databinding.FragmentRecipeDetailBinding
+import com.cmc.recipe.presentation.ui.base.BaseFragment
+
+
+class RecipeDetailFragment : BaseFragment<FragmentRecipeDetailBinding>(FragmentRecipeDetailBinding::inflate) {
+    override fun initFragment() {
+        TODO("Not yet implemented")
+    }
+}
