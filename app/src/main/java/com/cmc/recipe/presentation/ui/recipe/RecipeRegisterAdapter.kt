2826dd9cb41b1aa2f01dfa73d9fd00264176ae26
yydev10 <jym971024@gmail.com>
2023-07-20 02:16:36 +0900

    [#13] style : 레시피 작성 UI 구현

---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeRegisterAdapter.kt
@@ -0,0 +1,17 @@
+package com.cmc.recipe.presentation.ui.recipe
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cmc.recipe.data.model.RecipeItem
+import com.cmc.recipe.data.model.RecipeOrder
+import com.cmc.recipe.databinding.ItemRecipeOrderBinding
+import com.cmc.recipe.databinding.ItemRecipeRecommendBinding
+import com.cmc.recipe.presentation.ui.base.BaseAdapter
+import com.cmc.recipe.presentation.ui.base.BaseHolder
+import com.cmc.recipe.utils.loadImagesWithGlide
+
+class RecipeRegisterAdapter{
+
+}
---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeRegisterFragment.kt
@@ -0,0 +1,16 @@
+package com.cmc.recipe.presentation.ui.recipe
+
+import com.cmc.recipe.databinding.FragmentRecipeRegisterBinding
+import com.cmc.recipe.presentation.ui.base.BaseFragment
+
+class RecipeRegisterFragment : BaseFragment<FragmentRecipeRegisterBinding>(FragmentRecipeRegisterBinding::inflate) {
+
+    override fun initFragment() {
+        initRecipeRv()
+    }
+
+    private fun initRecipeRv(){
+
+    }
+
+}
