2ed5cfdbb9964336bf0e8259457ed5a88ae36457
yydev10 <jym971024@gmail.com>
2023-07-23 02:07:11 +0900

    [#13] fix : 레시피 작성 UI 레시피 입력 recyclerview 추가

--- app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeRegisterAdapter.kt
+++ app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeRegisterAdapter.kt
@@ -1,17 +1,33 @@
 package com.cmc.recipe.presentation.ui.recipe
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 import com.cmc.recipe.data.model.RecipeItem
-import com.cmc.recipe.data.model.RecipeOrder
-import com.cmc.recipe.databinding.ItemRecipeOrderBinding
-import com.cmc.recipe.databinding.ItemRecipeRecommendBinding
+import com.cmc.recipe.databinding.ItemRecipeStepBinding
 import com.cmc.recipe.presentation.ui.base.BaseAdapter
 import com.cmc.recipe.presentation.ui.base.BaseHolder
-import com.cmc.recipe.utils.loadImagesWithGlide
+import com.cmc.recipe.presentation.ui.base.OnClickListener
 
-class RecipeRegisterAdapter{
+class RecipeRegisterAdapter(val clickListener: OnClickListener):
+    BaseAdapter<String, ItemRecipeStepBinding, RecipeRegisterItemHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeRegisterItemHolder {
+        return RecipeRegisterItemHolder(
+            ItemRecipeStepBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+            clickListener
+        )
+    }
 
+    fun addItem(item: String) {
+        getData().add(item)
+        notifyItemInserted(getData().size - 1)
+    }
+}
+
+class RecipeRegisterItemHolder(viewBinding: ItemRecipeStepBinding, val clickListener: OnClickListener):
+    BaseHolder<String, ItemRecipeStepBinding>(viewBinding){
+    override fun bind(binding: ItemRecipeStepBinding, item: String?) {
+        binding.let { view->
+            view.tvStepTitle.text = item
+        }
+    }
 }
--- app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeRegisterFragment.kt
+++ app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeRegisterFragment.kt
@@ -1,16 +1,46 @@
 package com.cmc.recipe.presentation.ui.recipe
 
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.cmc.recipe.R
+import com.cmc.recipe.data.model.RecipeItem
 import com.cmc.recipe.databinding.FragmentRecipeRegisterBinding
 import com.cmc.recipe.presentation.ui.base.BaseFragment
+import com.cmc.recipe.presentation.ui.base.OnClickListener
 
 class RecipeRegisterFragment : BaseFragment<FragmentRecipeRegisterBinding>(FragmentRecipeRegisterBinding::inflate) {
 
     override fun initFragment() {
-        initRecipeRv()
+        val itemList = arrayListOf<String>()
+
+        initRecipeRv(itemList)
     }
 
-    private fun initRecipeRv(){
+    private fun initRecipeRv(itemList:ArrayList<String>){
+        val clickListener = object : OnClickListener {
+            override fun onMovePage(id: Int) {
+                findNavController().navigate(R.id.action_recipeMainFragment_to_recipeDetailFragment)
+            }
+        }
 
+        val adapter = RecipeRegisterAdapter(clickListener)
+        binding.rvStep.adapter = adapter
+        binding.rvStep.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+       // adapter.replaceData(itemList)
+
+        binding.etRecipe.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE || (event != null && event.keyCode == KeyEvent.KEYCODE_ENTER)) {
+                val recipeStep = binding.etRecipe.text.toString()
+                if (recipeStep.isNotEmpty()) {
+                    adapter.addItem(recipeStep)
+                    binding.etRecipe.text.clear() // EditText를 초기화합니다.
+                }
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
     }
 
 }
