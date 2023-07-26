8e423bda5b92d0e5932dfe761de29708e2cbce8a
yydev10 <jym971024@gmail.com>
2023-07-27 00:04:05 +0900

    [#13] style : 리뷰조회 페이지 UI구현

--- app/src/main/java/com/cmc/recipe/utils/ViewExtensions.kt
+++ app/src/main/java/com/cmc/recipe/utils/ViewExtensions.kt
@@ -11,6 +11,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import java.text.SimpleDateFormat
 
 fun ImageView.loadImagesWithGlide(url: String) {
     Glide.with(this)
@@ -18,4 +19,8 @@ fun ImageView.loadImagesWithGlide(url: String) {
         .centerCrop()
         .diskCacheStrategy(DiskCacheStrategy.ALL)
         .into(this)
+}
+
+fun String.convertTimestampToDate(time: Long){
+   SimpleDateFormat("yy.MM.dd").format(time).toString()
 }
