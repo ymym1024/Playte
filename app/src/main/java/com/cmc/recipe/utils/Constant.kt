8b95a4e94c22b8cd31012318af2905ebf7e69288
yydev10 <jym971024@gmail.com>
2023-08-03 02:34:57 +0900

    [#18] feat : tokenmanager 클래스 생성

--- app/src/main/java/com/cmc/recipe/utils/Constant.kt
+++ app/src/main/java/com/cmc/recipe/utils/Constant.kt
@@ -2,4 +2,7 @@ package com.cmc.recipe.utils
 
 object Constant {
     const val REQ_GALLERY = 100
+    const val PREFS_APP_FILE = "PREFS_APP_FILE"
+    const val USER_ACCESS_TOKEN = "USER_ACCESS_TOKEN"
+    const val USER_REFRESH_TOKEN = "USER_REFRESH_TOKEN"
 }
