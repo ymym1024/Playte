d3b8d0231b80a50aca216f67561dc0467e82fe73
yydev10 <jym971024@gmail.com>
2023-08-03 02:42:02 +0900

    [#18] fix : signData jwtToken data class로 변경

--- app/src/main/java/com/cmc/recipe/data/model/response/SignupResponse.kt
+++ app/src/main/java/com/cmc/recipe/data/model/response/SignupResponse.kt
@@ -3,10 +3,10 @@ package com.cmc.recipe.data.model.response
 data class SignupResponse(
     val code: String,
     val message: String,
-    val data : SignupData,
+    val data : jwtTokens,
 )
 
-data class SignupData(
+data class jwtTokens(
     val accessToken : String,
     val refreshToken : String
 )
