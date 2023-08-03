d2e4c39b4fbf91e0c81f05b99df64ba7c404c517
yydev10 <jym971024@gmail.com>
2023-08-04 02:12:21 +0900

    [#21] style : 숏폼 게시 UI 구현

---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/upload/UploadShortsFragment.kt
@@ -0,0 +1,13 @@
+package com.cmc.recipe.presentation.ui.upload
+
+import com.cmc.recipe.databinding.FragmentUploadShortsBinding
+import com.cmc.recipe.presentation.ui.base.BaseFragment
+
+class UploadShortsFragment : BaseFragment<FragmentUploadShortsBinding>(FragmentUploadShortsBinding::inflate) {
+
+    override fun initFragment() {
+
+    }
+
+
+}
