32d99911c171772a388ecb34da30b4d82e641b19
yydev10 <jym971024@gmail.com>
2023-08-03 23:17:59 +0900

    [#23] fix : bottom navigation mypage, upload fragment 추가

---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/mypage/MypageFragment.kt
@@ -0,0 +1,14 @@
+package com.cmc.recipe.presentation.ui.mypage
+
+
+import com.cmc.recipe.databinding.FragmentMypageBinding
+import com.cmc.recipe.presentation.ui.base.BaseFragment
+
+
+class MypageFragment : BaseFragment<FragmentMypageBinding>(FragmentMypageBinding::inflate) {
+
+    override fun initFragment() {
+
+    }
+
+}
---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/upload/UploadFragment.kt
@@ -0,0 +1,14 @@
+package com.cmc.recipe.presentation.ui.upload
+
+
+import com.cmc.recipe.databinding.FragmentMypageBinding
+import com.cmc.recipe.presentation.ui.base.BaseFragment
+
+
+class UploadFragment : BaseFragment<FragmentMypageBinding>(FragmentMypageBinding::inflate) {
+
+    override fun initFragment() {
+
+    }
+
+}
