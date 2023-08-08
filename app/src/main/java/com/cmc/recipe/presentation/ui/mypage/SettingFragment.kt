c111071b312a1d17aa2eaf3868953d7d3de16033
yydev10 <jym971024@gmail.com>
2023-08-09 03:01:31 +0900

    [#20] style : 설정 UI 구현

---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/mypage/SettingFragment.kt
@@ -0,0 +1,12 @@
+package com.cmc.recipe.presentation.ui.mypage
+
+import com.cmc.recipe.databinding.FragmentSettingBinding
+import com.cmc.recipe.presentation.ui.base.BaseFragment
+
+
+class SettingFragment : BaseFragment<FragmentSettingBinding>(FragmentSettingBinding::inflate) {
+    override fun initFragment() {
+
+    }
+
+}
