9df2be155932232770093de3cb558155e912632c
yydev10 <jym971024@gmail.com>
2023-08-09 00:01:27 +0900

    [#27] style : 검색 후 레시피 UI 구현

---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/search/SearchRecipeFragment.kt
@@ -0,0 +1,12 @@
+package com.cmc.recipe.presentation.ui.search
+
+import com.cmc.recipe.databinding.FragmentSearchRecipeBinding
+import com.cmc.recipe.presentation.ui.base.BaseFragment
+
+
+class SearchRecipeFragment : BaseFragment<FragmentSearchRecipeBinding>(FragmentSearchRecipeBinding::inflate) {
+    override fun initFragment() {
+
+    }
+
+}
---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/search/SearchShortsFragment.kt
@@ -0,0 +1,12 @@
+package com.cmc.recipe.presentation.ui.search
+
+import com.cmc.recipe.databinding.FragmentSearchShortsBinding
+import com.cmc.recipe.presentation.ui.base.BaseFragment
+
+
+class SearchShortsFragment : BaseFragment<FragmentSearchShortsBinding>(FragmentSearchShortsBinding::inflate) {
+    override fun initFragment() {
+
+    }
+
+}
