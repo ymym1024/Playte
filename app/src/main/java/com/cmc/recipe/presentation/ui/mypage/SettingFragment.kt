3670ebde053672a38f0e0e1fc7571d6b86258f37
yydev10 <jym971024@gmail.com>
2023-08-09 14:17:27 +0900

    [#20] style : Q&A, 이용약관 페이지 추가

--- app/src/main/java/com/cmc/recipe/presentation/ui/mypage/SettingFragment.kt
+++ app/src/main/java/com/cmc/recipe/presentation/ui/mypage/SettingFragment.kt
@@ -8,12 +8,48 @@ import com.cmc.recipe.presentation.ui.base.BaseFragment
 
 class SettingFragment : BaseFragment<FragmentSettingBinding>(FragmentSettingBinding::inflate) {
     override fun initFragment() {
-        binding.menuNick.setOnClickListener {
-            movePage(R.id.action_settingFragment_to_mypageNicknameFragment)
+
+        initView()
+        binding.let {
+            it.menuNick.setOnClickListener {
+                movePage(R.id.action_settingFragment_to_mypageNicknameFragment)
+            }
+
+            it.menuInfo.setOnClickListener {
+
+            }
+
+            it.menuTerms.setOnClickListener {
+                movePage(R.id.action_settingFragment_to_mypageTermsFragment)
+            }
+
+            it.menuQna.setOnClickListener {
+                movePage(R.id.action_settingFragment_to_mypageQnaFragment)
+            }
+
+            it.menuLogout.setOnClickListener {
+                logout()
+            }
+
+            it.menuWithdrawal.setOnClickListener {
+                withdrawal()
+            }
         }
+    }
+
+    private fun initView() {
+        binding.textView50.text = "CMC냉파"
     }
 
     private fun movePage(naviRes:Int){
         findNavController().navigate(naviRes)
+    }
+
+    private fun logout(){
+
+    }
+
+    private fun withdrawal(){
+
     }
 }
