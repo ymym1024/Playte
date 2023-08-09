311e0014fc21c85ab9e44cf4cc8d4c0d06556bbc
yydev10 <jym971024@gmail.com>
2023-08-09 12:47:57 +0900

    [#20] style : 닉네임 변경 UI 구현

---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/mypage/MypageNicknameFragment.kt
@@ -0,0 +1,11 @@
+package com.cmc.recipe.presentation.ui.mypage
+
+import com.cmc.recipe.databinding.FragmentMypageNicknameBinding
+import com.cmc.recipe.presentation.ui.base.BaseFragment
+
+class MypageNicknameFragment : BaseFragment<FragmentMypageNicknameBinding>(FragmentMypageNicknameBinding::inflate) {
+    override fun initFragment() {
+
+    }
+
+}
