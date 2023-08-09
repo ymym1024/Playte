8284560e4ada586dd885f49121d542062efe4a36
yydev10 <jym971024@gmail.com>
2023-08-10 02:30:07 +0900

    [#33] feat : 내 정보 조회 api 선언

--- app/src/main/java/com/cmc/recipe/data/source/remote/api/UserService.kt
+++ app/src/main/java/com/cmc/recipe/data/source/remote/api/UserService.kt
@@ -1,6 +1,7 @@
 package com.cmc.recipe.data.source.remote.api
 
 import com.cmc.recipe.data.model.response.BaseResponse
+import com.cmc.recipe.data.model.response.MyInfoResponse
 import com.cmc.recipe.data.source.remote.request.RequestNickname
 import retrofit2.Response
 import retrofit2.http.Body
@@ -10,4 +11,9 @@ interface UserService {
 
     @POST("/api/v1/users/verify-nickname")
     suspend fun verifyNickname(@Body nickname: RequestNickname) : Response<BaseResponse>
+
+    @POST("/api/v1/users/me")
+    suspend fun getMyInfo(@Body nickname: RequestNickname) : Response<MyInfoResponse>
+
+
 }
