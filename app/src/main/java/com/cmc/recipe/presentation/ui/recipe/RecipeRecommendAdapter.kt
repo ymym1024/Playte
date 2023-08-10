6e437e6c9c24a4044ba6bea9cea5d6a3ce53fd62
yydev10 <jym971024@gmail.com>
2023-08-10 22:12:45 +0900

    [#25] fix : 레시피 상세 추천 이미지 뷰 테두리 수정

--- app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeRecommendAdapter.kt
+++ app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeRecommendAdapter.kt
@@ -1,8 +1,12 @@
 package com.cmc.recipe.presentation.ui.recipe
 
 import android.content.Context
+import android.graphics.Outline
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.ViewOutlineProvider
+import com.cmc.recipe.R
 import com.cmc.recipe.data.model.RecipeItem
 import com.cmc.recipe.data.model.RecipeOrder
 import com.cmc.recipe.databinding.ItemRecipeOrderBinding
@@ -27,6 +31,15 @@ class RecipeRecommendItemHolder(viewBinding: ItemRecipeRecommendBinding):
         binding.let { view ->
             item?.let { recipe ->
                 view.ivThumbnail.loadImagesWithGlide(recipe.image_url)
+                view.ivThumbnail.outlineProvider = object : ViewOutlineProvider() {
+                    override fun getOutline(view: View, outline: Outline) {
+
+                        val cornerRadius = view.resources.getDimensionPixelSize(R.dimen.corner_radius) // 둥근 정도 조절
+                        outline.setRoundRect(0, 0, view.width, view.height+200, cornerRadius.toFloat())
+                    }
+                }
+
+                view.ivThumbnail.clipToOutline = true
                 view.tvRecommendTitle.text = recipe.name
                 view.tvRecommendTime.text = "조리시간 ${recipe.time}분"
             }
