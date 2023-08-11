a098622085baa65c00d563f22f9f5d722d951d7a
yydev10 <jym971024@gmail.com>
2023-08-11 22:40:44 +0900

    [#37] fix : 리뷰 UI 디자인 변경

--- app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeReviewFragment.kt
+++ app/src/main/java/com/cmc/recipe/presentation/ui/recipe/RecipeReviewFragment.kt
@@ -2,10 +2,20 @@ package com.cmc.recipe.presentation.ui.recipe
 
 import com.cmc.recipe.databinding.FragmentRecipeReviewBinding
 import com.cmc.recipe.presentation.ui.base.BaseFragment
+import com.cmc.recipe.utils.dpToPx
+import com.cmc.recipe.utils.loadImagesWithGlideRound
 
 class RecipeReviewFragment : BaseFragment<FragmentRecipeReviewBinding>(FragmentRecipeReviewBinding::inflate) {
     override fun initFragment() {
+        initView()
+    }
 
+    private fun initView(){
+        val url = "https://recipe1.ezmember.co.kr/cache/recipe/2022/02/02/dbb3f34bfe348a4bb4d142ff353815651.jpg" // TODO : 삭제예정
+        val pixel = dpToPx(requireContext(),100.toFloat())
+        binding.ivRecipeThumbnail.layoutParams.width = pixel
+        binding.ivRecipeThumbnail.layoutParams.height = pixel
+        binding.ivRecipeThumbnail.loadImagesWithGlideRound(url,10)
     }
 
 }
