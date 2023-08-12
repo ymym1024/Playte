29267080208063a6519ce05dbeddc954c0f4459c
yydev10 <jym971024@gmail.com>
2023-08-12 20:09:29 +0900

    [#38] fix : 재료 개수 다이얼로그 버튼 마이너스 disable 처리

--- app/src/main/java/com/cmc/recipe/presentation/ui/upload/IngredientCountDialog.kt
+++ app/src/main/java/com/cmc/recipe/presentation/ui/upload/IngredientCountDialog.kt
@@ -1,5 +1,6 @@
 package com.cmc.recipe.presentation.ui.upload
 
+import android.util.Log
 import com.cmc.recipe.R
 import com.cmc.recipe.databinding.DialogIngredientCountBinding
 import com.cmc.recipe.presentation.ui.base.BaseDialog
@@ -31,15 +32,18 @@ class IngredientCountDialog(val ingredient_name: String) : BaseDialog<DialogIngr
 
         binding.btnPlus.setOnClickListener {
             binding.tvCount.text = "${++count}"
+            if(count > 1){
+                binding.btnMinus.isEnabled = true
+            }
+            binding.btnMinus.setBackgroundResource(R.drawable.ic_minus_activate)
         }
 
         binding.btnMinus.setOnClickListener {
-            if(count > 1){
-                binding.btnMinus.setImageResource(R.drawable.ic_minus_activate)
-            }else{
-                binding.btnMinus.setImageResource(R.drawable.ic_minus_deactivate)
-            }
             binding.tvCount.text = "${--count}"
+            if(count == 1){
+                binding.btnMinus.setBackgroundResource(R.drawable.ic_minus_deactivate)
+                binding.btnMinus.isEnabled = false
+            }
         }
     }
 
