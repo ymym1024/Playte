5183d8412b664603a07503239a8761db645b2e26
yydev10 <jym971024@gmail.com>
2023-08-12 18:49:07 +0900

    [#38] feat : basedialog 생성

---
+++ app/src/main/java/com/cmc/recipe/presentation/ui/base/BaseDialog.kt
@@ -0,0 +1,72 @@
+package com.cmc.recipe.presentation.ui.base
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Insets
+import android.graphics.Point
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.DialogFragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseDialog<T: ViewBinding>(private val inflate: Inflate<T>) : DialogFragment(){
+    private var _binding: T? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate.invoke(inflater, container, false)
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+
+        initDialog()
+
+        return binding.root
+    }
+
+    abstract fun initDialog()
+
+    override fun onResume() {
+        super.onResume()
+        val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
+        val windowManager = activity?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val size = windowManager.currentWindowMetricsPointCompat()
+        val deviceWidth = size.x
+
+        params?.width = (deviceWidth * 0.8).toInt()
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
+    }
+
+    fun WindowManager.currentWindowMetricsPointCompat(): Point {
+        return if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R){
+            val windowInsets = currentWindowMetrics.windowInsets
+            var insets: Insets = windowInsets.getInsets(WindowInsets.Type.navigationBars())
+            windowInsets.displayCutout?.run {
+                insets = Insets.max(
+                    insets,
+                    Insets.of(safeInsetLeft,safeInsetTop,safeInsetRight,safeInsetBottom)
+                )
+            }
+            val insetsWidth = insets.right + insets.left
+            val insetsHeight = insets.top + insets.bottom
+            Point(
+                currentWindowMetrics.bounds.width() - insetsWidth,
+                currentWindowMetrics.bounds.height() - insetsHeight
+            )
+        }else{
+            Point().apply {
+                defaultDisplay.getSize(this)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
