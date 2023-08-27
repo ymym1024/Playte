7054060c8401050a3813b9d87247bd5306135d72
yydev10 <jym971024@gmail.com>
2023-08-27 22:49:18 +0900

    [#48] feat : 이용약관 화면 추가

--- app/src/main/java/com/cmc/recipe/presentation/ui/mypage/MypageTermsFragment.kt
+++ app/src/main/java/com/cmc/recipe/presentation/ui/mypage/MypageTermsFragment.kt
@@ -7,6 +7,10 @@ import com.cmc.recipe.presentation.ui.base.BaseFragment
 class MypageTermsFragment : BaseFragment<FragmentMypageTermsBinding>(FragmentMypageTermsBinding::inflate) {
     override fun initFragment() {
 
+        // 이용약관 다운로드
+        binding.tvDown.setOnClickListener {
+
+        }
     }
 
 }
