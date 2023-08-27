f96fa74787c83db84a6958aac84605cdb6163335
yydev10 <jym971024@gmail.com>
2023-08-27 16:45:39 +0900

    [#53] fix : 마이페이지 destination 변경

--- app/src/main/java/com/cmc/recipe/presentation/ui/mypage/MypageActivity.kt
+++ app/src/main/java/com/cmc/recipe/presentation/ui/mypage/MypageActivity.kt
@@ -2,6 +2,7 @@ package com.cmc.recipe.presentation.ui.mypage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
@@ -9,7 +10,9 @@ import com.cmc.recipe.R
 import com.cmc.recipe.databinding.ActivityMypageBinding
 import com.cmc.recipe.databinding.ActivityRecipeBinding
 import com.cmc.recipe.presentation.ui.recipe.RecipeActivityArgs
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MypageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMypageBinding
     private lateinit var navController: NavController
@@ -39,18 +42,19 @@ class MypageActivity : AppCompatActivity() {
         val graph = navController.navInflater.inflate(R.navigation.nav_mypage_graph)
         val args: MypageActivityArgs = MypageActivityArgs.fromBundle(intent.extras!!)
 
-        if (args.destination?.isNotBlank() == true) {
+        Log.d("destination","${args.destination}")
 
-            graph.setStartDestination(R.id.writeRecipeFragment)
-            navController.graph = graph
-
+        if (args.destination=="myReviewFragment") {
+            graph.setStartDestination(R.id.myReviewFragment)
             //navController.navigate(R.id.recipeThemeFragment, bundle, navOptions)
+        }else if(args.destination=="saveRecipeFragment"){
+            graph.setStartDestination(R.id.saveRecipeFragment)
+        }else if(args.destination=="settingFragment"){
+            graph.setStartDestination(R.id.settingFragment)
         } else {
             graph.setStartDestination(R.id.writeRecipeFragment)
-            navController.graph = graph
-
-            //navController.navigate(R.id.recipeDetailFragment, bundle, navOptions)
         }
+        navController.graph = graph
     }
 
     override fun onSupportNavigateUp(): Boolean {
