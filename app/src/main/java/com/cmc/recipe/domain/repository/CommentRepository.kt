2f7dd94fe80165c726409f8b29215f0b016882ae
yydev10 <jym971024@gmail.com>
2023-08-29 01:47:29 +0900

    [#47] feat : 댓글 관련 api 추가

---
+++ app/src/main/java/com/cmc/recipe/domain/repository/CommentRepository.kt
@@ -0,0 +1,24 @@
+package com.cmc.recipe.domain.repository
+
+import com.cmc.recipe.data.model.response.*
+import com.cmc.recipe.data.model.RecipeItem
+import com.cmc.recipe.data.model.entity.RecipeEntity
+import com.cmc.recipe.data.model.response.*
+import com.cmc.recipe.data.source.remote.request.CommentRequest
+import com.cmc.recipe.data.source.remote.request.ReviewRequest
+import com.cmc.recipe.utils.NetworkState
+import kotlinx.coroutines.flow.Flow
+
+interface CommentRepository {
+
+    fun getShortfromComment(id:Int) : Flow<NetworkState<CommentResponse>>
+
+    fun reportShortfromComment(id:Int) : Flow<NetworkState<BaseResponse>>
+
+    fun postShortfromCommentSave(id:Int,comment:CommentRequest) : Flow<NetworkState<BaseResponse>>
+
+    fun postShortfromCommentLike(id:Int) : Flow<NetworkState<BaseResponse>>
+
+    fun postShortfromCommentUnLike(id:Int) : Flow<NetworkState<BaseResponse>>
+
+}
---
+++ app/src/main/java/com/cmc/recipe/presentation/viewmodel/CommentViewModel.kt
@@ -0,0 +1,107 @@
+package com.cmc.recipe.presentation.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cmc.recipe.data.model.response.BaseResponse
+import com.cmc.recipe.data.model.response.CommentResponse
+import com.cmc.recipe.data.model.response.MyInfoResponse
+import com.cmc.recipe.data.source.remote.request.CommentRequest
+import com.cmc.recipe.data.source.remote.request.RequestNickname
+import com.cmc.recipe.domain.usecase.CommentUseCase
+import com.cmc.recipe.domain.usecase.UserUseCase
+import com.cmc.recipe.utils.NetworkState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CommentViewModel @Inject constructor(private val commentUseCase: CommentUseCase) : ViewModel() {
+
+    var _commentResult: MutableStateFlow<NetworkState<CommentResponse>> = MutableStateFlow(NetworkState.Loading)
+    var commentResult: StateFlow<NetworkState<CommentResponse>> = _commentResult
+
+    var _reportResult : MutableSharedFlow<NetworkState<BaseResponse>> = MutableSharedFlow()
+    var reportResult = _reportResult.asSharedFlow()
+
+    var _commentSaveResult : MutableSharedFlow<NetworkState<BaseResponse>> = MutableSharedFlow()
+    var commentSaveResult = _commentSaveResult.asSharedFlow()
+
+    var _commentLikeResult : MutableSharedFlow<NetworkState<BaseResponse>> = MutableSharedFlow()
+    var commentLikeResult = _commentLikeResult.asSharedFlow()
+
+    var _commentUnLikeResult : MutableSharedFlow<NetworkState<BaseResponse>> = MutableSharedFlow()
+    var commentUnLikeResult = _commentUnLikeResult.asSharedFlow()
+
+    fun getShortfromComment(id:Int) = viewModelScope.launch {
+        _commentResult.value = NetworkState.Loading
+        commentUseCase.getShortfromComment(id)
+            .catch { error ->
+                _commentResult.value = NetworkState.Error(400,"${error.message}")
+            }.collect { values ->
+                if (values is NetworkState.Error) {
+                    _commentResult.emit(NetworkState.Error(values.code,"${values.message}"))
+                } else if (values is NetworkState.Success) {
+                    _commentResult.emit(values)
+                }
+            }
+    }
+
+    fun reportShortfromComment(id:Int) = viewModelScope.launch {
+        _reportResult.emit(NetworkState.Loading)
+        commentUseCase.reportShortfromComment(id)
+            .catch { error ->
+                _reportResult.emit(NetworkState.Error(400,"${error.message}"))
+            }.collect { values ->
+                if (values is NetworkState.Error) {
+                    _reportResult.emit(NetworkState.Error(values.code,"${values.message}"))
+                } else if (values is NetworkState.Success) {
+                    _reportResult.emit(values)
+                }
+            }
+    }
+
+    fun postShortfromCommentSave(id:Int,comment:CommentRequest) = viewModelScope.launch {
+        _commentSaveResult.emit(NetworkState.Loading)
+        commentUseCase.postShortfromCommentSave(id,comment)
+            .catch { error ->
+                _commentSaveResult.emit(NetworkState.Error(400,"${error.message}"))
+            }.collect { values ->
+                if (values is NetworkState.Error) {
+                    _commentSaveResult.emit(NetworkState.Error(values.code,"${values.message}"))
+                } else if (values is NetworkState.Success) {
+                    _commentSaveResult.emit(values)
+                }
+            }
+    }
+
+    fun postShortfromCommentLike(id:Int) = viewModelScope.launch {
+        _commentLikeResult.emit(NetworkState.Loading)
+        commentUseCase.postShortfromCommentLike(id)
+            .catch { error ->
+                _commentLikeResult.emit(NetworkState.Error(400,"${error.message}"))
+            }.collect { values ->
+                if (values is NetworkState.Error) {
+                    _commentLikeResult.emit(NetworkState.Error(values.code,"${values.message}"))
+                } else if (values is NetworkState.Success) {
+                    _commentSaveResult.emit(values)
+                }
+            }
+    }
+
+    fun postShortfromCommentUnLike(id:Int) = viewModelScope.launch {
+        _commentUnLikeResult.emit(NetworkState.Loading)
+        commentUseCase.postShortfromCommentUnLike(id)
+            .catch { error ->
+                _commentUnLikeResult.emit(NetworkState.Error(400,"${error.message}"))
+            }.collect { values ->
+                if (values is NetworkState.Error) {
+                    _commentUnLikeResult.emit(NetworkState.Error(values.code,"${values.message}"))
+                } else if (values is NetworkState.Success) {
+                    _commentUnLikeResult.emit(values)
+                }
+            }
+    }
+
+}
